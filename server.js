8d76c7b46dfa2d5113fb727fa74f74abd7e72096
haithembenhalima <haithembenhalima12@gmail.com>
2024-09-04 23:09:11 +0100

    adding the json body limit

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(
 );
 app.use(express.urlencoded({extended:true}));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-app.use(express.json());
+app.use(express.json({limit: '3mb'}));
 app.use(cors());
 app.use(limiter);
 
